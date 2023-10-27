5b8c1e9704aef791359f842ef1a940977d76b175
Tran Minh Phuong <tmph2003@gmail.com>
2023-10-27 23:33:15 +0700

    first commit

---
+++ ForegroundAndBoundService/app/src/main/java/com/example/foregroundandboundservice/MyApplication.java
@@ -0,0 +1,24 @@
+package com.example.foregroundandboundservice;
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class MyApplication extends Application{
+    public static final String CHANNEL_ID = "CHANNEL_EXAMPLE";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "channel example", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            if(manager != null)
+                manager.createNotificationChannel(channel);
+        }
+    }
+}
